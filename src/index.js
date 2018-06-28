fb01c18e86eaf14e2594d4dcc194a0016c30c006
vtrushin <monospectr@gmail.com>
2018-06-29 01:50:21 +0300

    Changed render

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-import renderHtml from './render-html';
-import renderJsx from './render-jsx';
-
-// import { inspect } from 'util';
-// function log(object) {
-//     return console.log(inspect(object, {
-//         colors: true,
-//         depth: Infinity
-//     }));
-// }
-
-const wrapWithHtmlString = body => (
-`<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
-	<head>
-		<meta charset="utf-8" />
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
-		<style type="text/css">
-			{/*renderCss(cssList)*/}
-		</style>
-	</head>
-	${body}
-</html>`
-);
-
-export default parentJsxEl => {
-	return wrapWithHtmlString(
-		renderHtml(renderJsx(parentJsxEl))
-	);
-};
+export { default as render } from './render';
+export { default as createElement } from './create-element';
+export { default as Fragment } from './fragment';
